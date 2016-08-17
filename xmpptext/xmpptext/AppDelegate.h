37c1a712f458b2cba624d85a4379b4ecf529e517
zhouyongjian <MrZhouYongJian@163.com>
2016-08-17 14:10:11 +0800

    创建项目

---
+++ xmpptext/xmpptext/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xmpptext
+//
+//  Created by 周永建 on 16/8/17.
+//  Copyright © 2016年 xinde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xmpptext/xmpptext/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xmpptext
+//
+//  Created by 周永建 on 16/8/17.
+//  Copyright © 2016年 xinde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
